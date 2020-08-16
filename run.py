ea5d5d531ca4f9525904936c1ef1a98375d04892
iraperskova <67747336+iraperskova@users.noreply.github.com>
2020-08-16 06:14:36 +0300

    Add files via upload
    Частина 2

--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 from aiohttp import web
 
-
+BOOKS = [{
+  'id': 1,
+  'title': 'Test book 1',
+  'description': 'Test book 1 description'
+}, {
+  'id': 2,
+  'title': 'Test book 1',
+  'description': 'Test book 1 description'
+}, {
+  'id': 3,
+  'title': 'Test book 1',
+  'description': 'Test book 1 description'
+}]
 
 routes=web.RouteTableDef()
 
 @routes.get('/books')
 async def books_list(request):
-    return web.Response(text="books")
+    return web.json_response(BOOKS)
 
 app=web.Application()
 app.router.add_routes(routes)
